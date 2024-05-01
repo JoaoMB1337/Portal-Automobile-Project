2462dba29377d76fddd27137c89fb8fe2a91f491
João Barbosa <57194380+JoaoMB1337@users.noreply.github.com>
2024-05-01 19:00:01 +0100

    Remove Bootstrap

--- public/index.php
+++ public/index.php
@@ -12,6 +12,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// // Bootstrap Laravel and handle the request...
+// (require_once __DIR__.'/../bootstrap/app.php')
+//     ->handleRequest(Request::capture());
