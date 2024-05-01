b7d5caa5bb2b3585e5b2aaf8ab2e6712f49df0c8
João Barbosa <57194380+JoaoMB1337@users.noreply.github.com>
2024-05-01 19:10:19 +0100

    Update Database.php

--- config/database.php
+++ config/database.php
@@ -74,12 +74,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('RDS_HOSTNAME', '127.0.0.1'),
+            'port' => env('RDS_PORT', '5432'),
+            'database' => env('RDS_DB_NAME', 'ebdb'),
+            'username' => env('RDS_USERNAME', 'innodriveDB'),
+            'password' => env('RDS_PASSWORD', 'innodrive123.'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
