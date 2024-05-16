735afb5de4d1c50203ac1f0a2c4d2bb009ed23a9
Diogo Sousa <diogosousainf@gmail.com>
2024-05-16 23:08:11 +0100

    employees-vehicles-p1

--- resources/views/components/Vehicles/create-vehicles.blade.php
+++ resources/views/components/Vehicles/create-vehicles.blade.php
@@ -117,7 +117,7 @@
 
             <div>
                 <label for="notes" class="block text-sm font-semibold text-gray-700 mb-2">Notas</label>
-                <textarea id="notes" class="form-textarea w-full rounded-md border-gray-300 focus:border-gray-400 focus:ring focus:ring-gray-200 @error('notes') border-red-500 @enderror" name="notes" required autocomplete="notes" rows="4">{{ old('notes') }}</textarea>
+                <textarea id="notes" class="form-textarea w-full rounded-md border-gray-300 focus:border-gray-400 focus:ring focus:ring-gray-200 @error('notes') border-red-500 @enderror" name="notes"  autocomplete="notes" rows="4">{{ old('notes') }}</textarea>
                 @error('notes')
                 <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
