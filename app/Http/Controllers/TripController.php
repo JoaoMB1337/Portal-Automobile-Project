e25faf7352d360a1c3fb1169c194dea072eb4b5c
João Barbosa <57194380+JoaoMB1337@users.noreply.github.com>
2024-05-28 20:22:36 +0100

    fix:edit

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -116,25 +116,9 @@ class TripController extends Controller
      * Update the specified resource in storage.
      */
 
-    public function update(StoreTripRequest $request, $id)
+    public function update(Request $request, $id)
     {
         $trip = Trip::findOrFail($id);
-        $trip->start_date = $request->start_date;
-        $trip->end_date = $request->end_date;
-        $trip->destination = $request->destination;
-        $trip->purpose = $request->purpose;
-        $trip->project_id = $request->project_id;
-        $trip->type_trip_id = $request->type_trip_id;
-        $trip->save();
-
-        $trip->employees()->sync([$request->employee_id]);
-        $trip->vehicles()->sync([$request->vehicle_id]);
-
-        return redirect()->route('trips.index');
-    }
-
-    /* public function update(UpdateTripRequest $request, Trip $trip)
-    {
         $trip->start_date = $request->start_date;
         $trip->end_date = $request->end_date;
         $trip->destination = $request->destination;
@@ -144,12 +128,12 @@ class TripController extends Controller
         $trip->vehicle_id = $request->vehicle_id;
         $trip->save();
 
-        // Atualizar associações empregado-viagem
-        $trip->employees()->sync($request->employee_id);
-        $trip->vehicles()->sync($request->vehicle_id);
+        $trip->employees()->sync([$request->employee_id]);
+        $trip->vehicles()->sync([$request->vehicle_id]);
 
         return redirect()->route('trips.index');
-    } */
+    }
+     
 
     /**
      * Remove the specified resource from storage.
