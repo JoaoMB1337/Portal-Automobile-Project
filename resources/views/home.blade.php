e75036468c69e9695dab205c3453c5650f5f3481
Diogo Sousa <diogosousainf@gmail.com>
2024-06-06 22:08:38 +0100

    feat:dashboard-done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,57 +1,52 @@
 @extends('components.master.main')
+@section('content')
 
 @if(Auth::check() && Auth::user()->employee_role_id == 2)
 
-
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 <body class="bg-gray-100 h-screen">
 <div class="lg:pl-64 overflow-x-auto">
     <div class="container mx-auto p-4">
-        <div class="p-4">
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-4">
+        <div class="mt-6">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
                 <!-- Perfil do usuário -->
-                <div class="bg-white rounded-md border border-gray-100 p-4 shadow-md shadow-black/5">
-                    <div class="flex justify-between mb-4">
+                <div class="bg-white rounded-lg border border-gray-200 p-6 shadow-sm">
+                    <div class="flex flex-col justify-between mb-4">
                         <div>
-                            <div class="flex items-center mb-1">
+                            <div class="flex items-center mb-4">
                                 <div class="font-semibold">
-                                    <p class="text-gray-800 font-bold">{{ Auth::user()->name }}</p>
-                                    <p class="text-gray-600">{{ Auth::user()->employee_number }}</p>
-                                    <p class="text-gray-600">{{ Auth::user()->role->name }}</p>
+                                    <p class="text-gray-900 text-lg font-bold">{{ Auth::user()->name }}</p>
+                                    <p class="text-gray-700">{{ Auth::user()->employee_number }}</p>
+                                    <p class="text-gray-700">{{ Auth::user()->role->name }}</p>
                                     <br>
-                                    <a href="{{ route('employees.show', Auth::user()->id) }}" class="text-[#f84525] font-medium text-sm hover:text-red-800">Show profile</a>
+                                    <a href="{{ route('employees.show', Auth::user()->id) }}" class="text-[#f84525] font-medium text-sm hover:text-red-700">Show profile</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <!-- Seção Viagens -->
-                <div class="bg-white rounded-md border border-gray-100 p-4 shadow-md shadow-black/5">
-                    <h2 class="text-xl font-semibold mb-4">Viagens</h2>
-                    <h2 class="text-center text-2xl">{{ $totalTrips }}</h2>
+                <div class="bg-white rounded-lg border border-gray-200 p-6 shadow-sm">
+                    <h2 class="text-2xl font-semibold mb-4 text-gray-900">Viagens </h2>
+                    <h2 class="text-center text-4xl font-bold text-gray-900">{{ $totalTrips }}</h2>
+                </div>
+            </div>
+
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <!-- Seção Veículos -->
+                <div class="bg-white p-6 rounded-lg shadow-sm">
+                    <h2 class="text-2xl font-semibold mb-4 text-gray-900">Veículos</h2>
+                    <canvas id="vehiclesChart" class="w-full h-full"></canvas>
+                </div>
+
+                <!-- Seção Projetos -->
+                <div class="bg-white p-6 rounded-lg shadow-sm col-span-2">
+                    <h2 class="text-2xl font-semibold mb-4 text-gray-900">Projetos</h2>
+                    <canvas id="projectsChart" class="w-full h-full"></canvas>
                 </div>
             </div>
         </div>
-
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <!-- Seção Veículos -->
-            <!-- Seção Veículos -->
-            <div class="bg-white p-4 rounded-lg shadow-md">
-                <h2 class="text-xl font-semibold mb-4">Veículos</h2>
-                <canvas id="vehiclesChart" style="width: 200px; height: 200px;"></canvas>
-            </div>
-
-
-
-            <!-- Seção Projetos -->
-            <div class="bg-white p-2 rounded-md shadow-md">
-                <h2 class="text-lg font-semibold mb-2">Projetos</h2>
-                <canvas id="projectsChart"></canvas>
-            </div>
-        </div>
-    </div>
-</div>
-
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         // Dados dos veículos
@@ -67,7 +62,7 @@
                 labels: ['Internos', 'Externos'],
                 datasets: [{
                     data: [vehiclesData.internal, vehiclesData.external],
-                    backgroundColor: ['#4CAF50', '#FF5733']
+                    backgroundColor: ['#37afa5', '#1b3342']
                 }]
             }
         });
@@ -96,11 +91,11 @@
                         projectsData.onHold
                     ],
                     backgroundColor: [
-                        '#FF6384',
+                        '#1b3342',
                         '#36A2EB',
-                        '#FFCE56',
-                        '#E7E9ED',
-                        '#4BC0C0'
+                        '#37afa5',
+                        '#c1c3c6',
+                        '#f83038'
                     ],
                     borderWidth: 1
                 }]
@@ -110,16 +105,20 @@
                     y: {
                         beginAtZero: true
                     }
+                },
+                plugins: {
+                    legend: {
+                        display: false
+                    }
                 }
             }
         });
+
     });
 </script>
-</body>
-</html>
+</div>
 
 @else
-
     <div class="mx-auto pl-10 lg:pl-64">
         <div class="p-6">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
@@ -142,3 +141,5 @@
         </div>
     </div>
 @endif
+
+@endsection
