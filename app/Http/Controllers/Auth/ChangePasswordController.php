4be4d57b362b64c002608dc62773814f9602ae4b
Marcos Carvalho <140098118+MarcosBC1994@users.noreply.github.com>
2024-06-20 09:11:53 +0100

    first-login-done
    adicionado changePassword Controller
                      middleware CheckFristLogin
                      views/auth change.php
    
    alterações feitas no migrations factory e seeder employee e auth routes

---
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class ChangePasswordController extends Controller
+{
+    public function showChangePasswordForm()
+    {
+        return view('auth.passwords.change');
+    }
+
+    public function changePassword(Request $request)
+    {
+        $request->validate([
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = Auth::user();
+        $user->password = Hash::make($request->password);
+        $user->first_login = false;
+        $user->save();
+
+        return redirect()->route('home')->with('success', 'Password changed successfully.');
+    }
+}
