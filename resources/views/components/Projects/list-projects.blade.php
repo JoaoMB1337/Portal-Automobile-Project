e2f7754c7538ecb5a9173f87cb94e8e764e9ec73
Diogo Sousa <diogosousainf@gmail.com>
2024-07-11 18:26:29 +0100

    projects and trips

--- resources/views/components/Projects/list-projects.blade.php
+++ resources/views/components/Projects/list-projects.blade.php
@@ -4,9 +4,21 @@
     <div class="p-4 md:p-6 rounded-lg shadow-md mb-3 bg-white">
         <div class="flex flex-col md:flex-row items-center justify-between mb-4 md:mb-6">
             <div class="flex items-center space-x-2 md:space-x-4 mb-4 md:mb-0">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-700">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                </svg>
+                <svg class="w-8 h-8 text-gray-700" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 505 505" xml:space="preserve" fill="#000000">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <circle style="fill:#324A5E;" cx="252.5" cy="252.5" r="252.5"></circle>
+                        <rect x="161.2" y="139.1" style="fill:#E6E9EE;" width="218.3" height="262.6"></rect>
+                        <rect x="125.5" y="103.3" style="fill:#FFFFFF;" width="218.3" height="262.6"></rect>
+                        <g>
+                            <rect x="149.5" y="143.3" style="fill:#4CDBC4;" width="170.2" height="16.2"></rect>
+                            <rect x="149.5" y="198.8" style="fill:#4CDBC4;" width="170.2" height="16.2"></rect>
+                            <rect x="149.5" y="254.3" style="fill:#4CDBC4;" width="170.2" height="16.2"></rect>
+                            <rect x="149.5" y="309.8" style="fill:#4CDBC4;" width="170.2" height="16.2"></rect>
+                        </g>
+                    </g>
+            </svg>
                 <h1 class="text-lg md:text-2xl font-semibold text-gray-900">Projetos</h1>
             </div>
             <div class="flex flex-col space-y-2 w-full md:flex-row md:space-x-4 md:space-y-0 md:w-auto">
@@ -20,6 +32,7 @@
             </div>
         </div>
     </div>
+
 
 
     @if(Auth::check() && Auth::user()->isMaster())
