2cae6bb14f97864bcbe8c583b113f4a537105989
Diogo Sousa <diogosousainf@gmail.com>
2024-07-16 22:47:23 +0100

    modal done

--- resources/views/home-employee.blade.php
+++ resources/views/home-employee.blade.php
@@ -8,7 +8,7 @@
                             <p class="text-gray-900 text-lg font-bold">Bem-vindo</p>
                             <p>{{ Auth::user()->name }}</p>
                             <p class="text-gray-700 mt-2">{{ Auth::user()->employee_number }}</p>
-                            <a href="{{ route('employees.show', Auth::user()->id) }}" class="text-[#f84525] font-medium text-sm hover:text-red-700 mt-3">Show profile</a>
+                            <a href="{{ route('employees.show', Auth::user()->id) }}" class="text-[#f84525] font-medium text-sm hover:text-red-700 mt-3">Ver Perfil</a>
                         </div>
                     </div>
                 </div>
@@ -28,43 +28,28 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
-                    @forelse ($activeTrips as $trip)
-                        <tr>
-                            <td class="py-4 whitespace-nowrap text-center text-gray-800 cursor-pointer" onclick="showTripDetails('{{ $trip->id }}', '{{ $trip->start_date }}', '{{ $trip->end_date }}', '{{ $trip->destination }}', '{{ $trip->details }}')">
-                                {{ $trip->start_date }}
-                            </td>
-                            <td class="py-4 whitespace-nowrap text-center text-gray-800 cursor-pointer" onclick="showTripDetails('{{ $trip->id }}', '{{ $trip->start_date }}', '{{ $trip->end_date }}', '{{ $trip->destination }}', '{{ $trip->details }}')">
-                                {{ $trip->destination }}
-                            </td>
-
-                            <td>
-                                @foreach ($trip->vehicles as $vehicle)
-                                    {{ $vehicle->plate }}
-                                @endforeach
-                            </td>
-                            <td>
-                                {{$trip->project->country->name}}
-                            </td>
-                            <td>
-
-                                @if($trip->project->district)
-                                    {{$trip->project->district->name}}
-                                @else
-                                    <p>Sem Distrito</p>
-                                @endif
-                            </td>
+                @forelse ($activeTrips as $trip)
+                    <tr>
+                        <td class="py-4 whitespace-nowrap text-center text-gray-800 cursor-pointer" onclick="showTripDetails('{{ $trip->id }}', '{{ $trip->start_date }}', '{{ $trip->end_date }}', '{{ $trip->destination }}', '{{ implode(', ', $trip->vehicles->pluck('plate')->toArray()) }}', '{{ $trip->project->country->name }}', '{{ $trip->project->district->name ?? 'N/A' }}')">
+                            {{ $trip->start_date }}
+                        </td>
+                        <td class="py-4 whitespace-nowrap text-center text-gray-800 cursor-pointer" onclick="showTripDetails('{{ $trip->id }}', '{{ $trip->start_date }}', '{{ $trip->end_date }}', '{{ $trip->destination }}', '{{ implode(', ', $trip->vehicles->pluck('plate')->toArray()) }}', '{{ $trip->project->country->name }}', '{{ $trip->project->district->name ?? 'N/A' }}')">
+                            {{ $trip->destination }}
+                        </td>
 
 
-                            <td class="py-4 whitespace-nowrap text-center text-gray-800">
-                                <a href="{{ route('trip-details.create', ['trip_id' => $trip->id]) }}" class="bg-green-700 hover:bg-green-600 text-white font-medium text-sm px-3 py-2 rounded">Adicionar Custo</a>
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="3" class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium text-gray-500">Nenhuma viagem atribuída hoje.</td>
-                        </tr>
-                    @endforelse
+
+                        <td class="py-4 whitespace-nowrap text-center text-gray-800">
+                            <a href="{{ route('trip-details.create', ['trip_id' => $trip->id]) }}" class="bg-green-700 hover:bg-green-600 text-white font-medium text-sm px-3 py-2 rounded">Adicionar Custo</a>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="6" class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium text-gray-500">Nenhuma viagem atribuída hoje.</td>
+                    </tr>
+                @endforelse
                 </tbody>
+
 
             </table>
             <div class="py-5 flex justify-center">
