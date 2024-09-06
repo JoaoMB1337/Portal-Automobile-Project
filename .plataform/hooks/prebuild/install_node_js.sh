880c1e136b9389ed77c449ac16e473555942d5c0
JoaoMB1337 <joaobarbosa152002@gmail.com>
2024-09-06 14:24:18 +0100

    update install node

--- .plataform/hooks/prebuild/install_node_js.sh
+++ .plataform/hooks/prebuild/install_node_js.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-if [[ ! "$(node - version)" =~ "v16" ]]; then
-sudo yum install -y gcc-c++ make
-sudo yum remove -y nodejs npm
-sudo rm -rf /var/cache/yum/*
-sudo yum clean all
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
-sudo yum install -y nodejs
+if [[ ! "$(node -v)" =~ "v16" ]]; then
+  sudo yum install -y gcc-c++ make
+  sudo yum remove -y nodejs npm
+  sudo rm -rf /var/cache/yum/*
+  sudo yum clean all
+  curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+  sudo yum install -y nodejs
+fi
