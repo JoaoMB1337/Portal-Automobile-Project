0521efb966d6ffad127ccee0be322722a91f86b1
JoaoMB1337 <joaobarbosa152002@gmail.com>
2024-10-03 16:01:11 +0100

    try update

--- .plataform/hooks/prebuild/install_node_js.sh
+++ .plataform/hooks/prebuild/install_node_js.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [[ ! "$(node -v)" =~ "v16" ]]; then
+if [[ ! "$(node -v)" =~ "v20" ]]; then
   sudo yum install -y gcc-c++ make
   sudo yum remove -y nodejs npm
   sudo rm -rf /var/cache/yum/*
   sudo yum clean all
-  curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+  curl -sL https://rpm.nodesource.com/setup_20.x | sudo -E bash -
   sudo yum install -y nodejs
 fi
